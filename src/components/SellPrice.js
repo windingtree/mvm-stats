6bd2282069840fb2293331fae0d147c7a9867d43
skkr <nico.sikker@gmail.com>
2018-04-03 22:53:48 -0300

    New css helper for text size. Reuced the size of chart leabels.

--- src/components/SellPrice.js
+++ src/components/SellPrice.js
@@ -9,7 +9,7 @@ const SellPrice = props => {
       <b className="d-block d-sm-inline text-center text-sm-left">{sellPriceTitle}</b>
       <hr className="d-block d-sm-none mt-1 mb-2"/>
       <div className="text-center text-sm-left float-sm-right">
-        <b className="ml-0 ml-sm-3">USD:</b> {parseFloat(sellPriceUSD).toFixed(6)}
+        <b className="ml-0 ml-sm-3 text--xs">USD:</b> {parseFloat(sellPriceUSD).toFixed(6)}
         <span className="ml-3 text-light">|</span>
         <b className="ml-3 text--xs">ETH:</b> {parseFloat(sellPriceETH).toFixed(6)}
       </div>
